249047c9b9cf703769a39bb721c28fec430157f2
reddyzhub <abhilashz.de@gmail.com>
2024-07-09 10:18:10 +0200

    Added comments to Reposioty interface

--- src/main/java/com/example/devicemanagement/repository/DeviceRepository.java
+++ src/main/java/com/example/devicemanagement/repository/DeviceRepository.java
@@ -6,7 +6,19 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The DeviceRepository interface extends JpaRepository to provide CRUD operations for the Device entity.
+ * It also includes a custom query method to find devices by their brand.
+ */
+
 @Repository
 public interface DeviceRepository extends JpaRepository<Device, Long> {
+    /**
+     * Finds a list of devices by their brand.
+     *
+     * @param brand the brand of the devices to find
+     * @return a list of devices with the specified brand
+     */
+
     List<Device> findByBrand(String brand);
 }
