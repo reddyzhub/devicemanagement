d7d210c9463d21584c47cc52af5ce216c0807929
reddyzhub <abhilashz.de@gmail.com>
2024-07-09 11:18:43 +0200

    Added comments to Sec Config

--- src/main/java/com/example/devicemanagement/config/SecurityConfiguration.java
+++ src/main/java/com/example/devicemanagement/config/SecurityConfiguration.java
@@ -19,8 +19,9 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        httpSecurity.csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection for simplicity
                 .authorizeHttpRequests(authorize -> {
+                    // Permit all requests to the Swagger UI, API docs, and H2 console
                     authorize.requestMatchers(antMatcher("/swagger-ui/**"),
                                     antMatcher("/swagger-ui.html"),
                                     antMatcher("/v3/**"),
@@ -29,13 +30,14 @@ public class SecurityConfiguration {
                             .requestMatchers(antMatcher(HttpMethod.GET, "/devices/**")).permitAll()
                             .requestMatchers(antMatcher(HttpMethod.PUT, "/devices/**")).permitAll()
                             .requestMatchers(antMatcher(HttpMethod.POST, "/devices/**")).permitAll()
+                            .requestMatchers(antMatcher(HttpMethod.PATCH, "/devices/**")).permitAll()
                             .requestMatchers(antMatcher(HttpMethod.DELETE, "/devices/**")).permitAll()
-                            .anyRequest().authenticated();
+                            .anyRequest().authenticated(); // Require authentication for any other requests
                 })
                 .headers(h -> h.frameOptions(
-                        HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // This so embedded frames in h2-console are working
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        HeadersConfigurer.FrameOptionsConfig::sameOrigin)) // Allow H2 console to be embedded in a frame
+                .httpBasic(Customizer.withDefaults()) // Use basic HTTP authentication
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Use stateless session management
 
         return httpSecurity.build();
     }
