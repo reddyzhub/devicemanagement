1de40a8aaa6106a8ac65e6b880660d6fd341ce79
reddyzhub <abhilashz.de@gmail.com>
2024-07-09 10:31:53 +0200

    Added comments to DeviceDTO

--- src/main/java/com/example/devicemanagement/dto/DeviceDTO.java
+++ src/main/java/com/example/devicemanagement/dto/DeviceDTO.java
@@ -5,12 +5,27 @@ import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-
+/**
+ * Data Transfer Object (DTO) for Device.
+ *
+ * DTOs are used to transfer data between layers of an application.
+ * They help decouple the service layer from the presentation layer,
+ * enhance security by exposing only necessary data, and include validation.
+ *
+ * This class follows the Data Transfer Object (DTO) pattern.
+ */
 public record DeviceDTO(
         Long id,
         @NotEmpty(message = "Name is mandatory") String name,
         @NotEmpty(message = "Brand is mandatory") String brand,
         LocalDateTime creationTime) {
+    /**
+     * Updates the fields of the DeviceDTO with the values from the provided map.
+     * If a value is not present in the map, the existing value is retained.
+     *
+     * @param updates A map containing the fields to update.
+     * @return A new DeviceDTO instance with the updated fields.
+     */
 
     public DeviceDTO withUpdates(Map<String, Object> updates) {
         String newName = updates.getOrDefault("name", name) != null ? (String) updates.get("name") : name;
